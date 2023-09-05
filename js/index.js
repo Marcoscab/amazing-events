18ca2b5873b2a43828f49a5c0e61cda05dabb727
Marcoscab <marcos_stritzke@hotmail.com>
2023-09-04 22:41:15 -0300

    aplicando filtros con checkbox V1

--- js/index.js
+++ js/index.js
@@ -1,14 +1,33 @@
+/*------------------------------------------------------------------*/
+/*----------------------------VARIABLES----------------------------*/
+
 /*Recuperamos los elementos a modificar dinamicamente */
 let $carruselItem = document.getElementById("carrusel");
 let $botoneraCarrusel = document.getElementById("botoneraCarrusel");
-let $tarjetas = document.getElementById("tarjetas")
+let $tarjetas = document.getElementById("tarjetas");
+let $checkbox = document.getElementById("checkboxFiltros");
 
 /*recupero los eventos de la DB*/
 let eventos = data.events;
 
 let carruselItem = "";//Aca se guarda el carrusel item con referencia a la imagen de cada evento
-let tarjetas = ""// Aca se guardan las tarjetas
 let botones = ""; // Aca se guardan los botones del carrusel.
+let checkboxs = ""; // Aca se guardan los checkbox creados
+let setDeChecks = new Set(); // Lista donde se guardan los checks marcados.
+
+/* Recuperamos todas las categorias y las guardamos en un array. Para esto usamos la funcion map
+que me devuelve un array con el return de la funcion flecha */
+let categorias = eventos.map((evento) => { return evento.category });
+
+/*Para borrar los duplicados creo un nuevo array con un Set q no permite duplicados pasando 
+por argunmento el array de categorias. A su ves con el metodo from lo convierto a array nuevamente*/
+let categoriasFiltrada = Array.from(new Set(categorias));
+
+/*-------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------*/
+/*----------------------------FUNCIONES----------------------------*/
 
 /*Funcion que devuelve el HTML para las imagenes del carrusel*/
 function templateCarrusel(eventos, tiempo) {
@@ -29,9 +48,9 @@ function templateCarrusel(eventos, tiempo) {
                                 </div>
                             </div>`;
         }
-  
+
     }
-          return carruselItem;
+    return carruselItem;
 };
 
 /*Funcion que devuelve el HTML para los botones del carrusel*/
@@ -50,8 +69,9 @@ function templateBotoneraCarrusel(eventos) {
     return botones;
 }
 
-/*Funcion que devuelve el HTML para las tarjetas*/
-function templateTarjetas(eventos) {
+/*Funcion que inserta las tarjetas en el HTML en base a los eventos recibidos*/
+function insertarTarjetas(eventos, contenedorTarjetas) {
+    let tarjetas="";
     for (let i = 0; i < eventos.length; i++) {
 
         if (i == 0) {
@@ -82,15 +102,65 @@ function templateTarjetas(eventos) {
                         </div>`;
         }
     }
-    return tarjetas;
+    //Cargamos las tarjetas.
+    contenedorTarjetas.innerHTML = tarjetas;
+};
 
+//Funcion q crea el codigo HTML de los checkbox dinamicamente.
+function templateCheckbox(categorias) {
+    console.log(categorias);
+    for (let i = 0; i < categorias.length; i++) {
+        checkboxs += `
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" id="${categorias[i]}" value="${categorias[i]}">
+            <label class="form-check-label" for="${categorias[i]}">${categorias[i]}</label>
+        </div>`;
+    }
+    return checkboxs;
 }
 
+/* Ahora creamos el evento para filtrar las tarjetas cuando se selecciona un checkbox.*/
+function filtrarTarjeta(e,eventos,contenedorTarjetas,setchk) {
+    if (e.target.checked) {
+        setchk.add(e.target.value);
+    } else {
+        setchk.delete(e.target.value);
+    };
+    //convierto el Set a Array.
+    let arrayDeChecks = Array.from(setchk);
+    if(arrayDeChecks.length!=0){
+        let tarFilt = eventos.filter((evento) => { return arrayDeChecks.indexOf(evento.category) != -1 });
+        insertarTarjetas(tarFilt,contenedorTarjetas);
+    }else{
+        insertarTarjetas(eventos,contenedorTarjetas);//Si no hay ningun filtro seleccionado muestro todos los checks.
+    };
+};
+
+/*-------------------------------------------------------------------*/
 
 /*Colocamos los componentes creados en HTML.*/
 $botoneraCarrusel.innerHTML = templateBotoneraCarrusel(eventos);
 $carruselItem.innerHTML = templateCarrusel(eventos, "HOME");
-$tarjetas.innerHTML = templateTarjetas(eventos);
+$checkbox.innerHTML = templateCheckbox(categoriasFiltrada);
+insertarTarjetas(eventos,$tarjetas);
+
+
+
+/*------------------------------------------------------------------*/
+/*----------------------------ACTION LISTENERS----------------------------*/
+$checkbox.addEventListener("click", (e)=>{filtrarTarjeta(e,eventos,$tarjetas,setDeChecks)});
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
