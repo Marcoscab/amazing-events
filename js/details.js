942e9ab6d20d6a1cb5a95cc38e4ca71e52738d7c
Marcoscab <marcos_stritzke@hotmail.com>
2023-09-06 21:52:11 -0300

    Se termina index y details Spring 3

---
+++ js/details.js
@@ -0,0 +1,56 @@
+/*----------------------------VARIABLES----------------------------*/
+
+const $tarjeta = document.getElementById("tarjetaDetallada");
+let eventos = data.events; // Guardo los datos de la DB
+const objetoURL = new URLSearchParams(location.search);//Obtengo el objeto url donde se guarda los datos de q viajan en la URL
+const id = objetoURL.get("id");//Obtengo el Id que viaja en la URL utilizando par clave valor.
+
+/*----------------------------FIN VARIABLES------------------------*/
+
+
+/*----------------------------FUNCIONES----------------------------*/
+
+//Funcion: busca el evento segun Id y lo devuelve
+function buscarEvento(eventos, id) {
+    let evento = eventos.find((evento) => { return evento._id === id });
+    //console.log(evento);
+    return evento;
+}
+
+function templateTarjeta(evento) {
+    let tarjeta = `
+    <div class="row g-0 align-items-center">
+        <div class="col-md-4">
+            <img src="${evento.image}" class="img-fluid rounded-start" alt="Imagen de ${evento.name}">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title text-center mb-0">${evento.name}</h5>
+                <p class="card-text mb-0">${evento.description}</p>
+                <p class="card-text mb-0"><strong>Lugar: </strong>${evento.place}</p>
+                <p class="card-text mb-0"><strong>Capacidad: </strong>${evento.capacity}</p>
+                <p class="card-text mb-0"><strong>Precio: $</strong>${evento.price}</p>
+            </div>
+        </div>
+    </div>`
+
+    return tarjeta;
+
+};
+
+
+//Funcion para insertar componentes
+function insertarComponente(component, template) {
+    component.innerHTML = template;
+};
+
+/*----------------------------FIN FUNCIONES------------------------*/
+
+
+/*--------------------LLAMADO FUNCIONES ARRANQUE----------------------------*/
+let evento = buscarEvento(eventos, id);//Buscamos el evento segun la id recibida en la URL
+let template = templateTarjeta(evento);
+insertarComponente($tarjeta,template);
+
+
+/*--------------------FIN LLAMADO FUNCIONES ARRANQUE------------------------*/
--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-/*------------------------------------------------------------------*/
+
 /*----------------------------VARIABLES----------------------------*/
 
 /*Recuperamos los elementos a modificar dinamicamente */
@@ -6,55 +6,45 @@ let $carruselItem = document.getElementById("carrusel");
 let $botoneraCarrusel = document.getElementById("botoneraCarrusel");
 let $tarjetas = document.getElementById("tarjetas");
 let $checkbox = document.getElementById("checkboxFiltros");
+let $serchBar = document.getElementById("serchBar");
 
-/*recupero los eventos de la DB*/
-let eventos = data.events;
-
-let carruselItem = "";//Aca se guarda el carrusel item con referencia a la imagen de cada evento
-let botones = ""; // Aca se guardan los botones del carrusel.
-let checkboxs = ""; // Aca se guardan los checkbox creados
+let eventos = data.events; // GUardo los datos de la DB
 let setDeChecks = new Set(); // Lista donde se guardan los checks marcados.
 
-/* Recuperamos todas las categorias y las guardamos en un array. Para esto usamos la funcion map
-que me devuelve un array con el return de la funcion flecha */
-let categorias = eventos.map((evento) => { return evento.category });
-
-/*Para borrar los duplicados creo un nuevo array con un Set q no permite duplicados pasando 
-por argunmento el array de categorias. A su ves con el metodo from lo convierto a array nuevamente*/
-let categoriasFiltrada = Array.from(new Set(categorias));
-
-/*-------------------------------------------------------------------*/
+/*-----------------------------FIN VARIABLES---------------------------*/
 
 
-/*------------------------------------------------------------------*/
+
 /*----------------------------FUNCIONES----------------------------*/
 
-/*Funcion que devuelve el HTML para las imagenes del carrusel*/
-function templateCarrusel(eventos, tiempo) {
+//Funcion: Crea el HTML para las imagenes del carrusel y llama a la funcion para insertarlo
+function templateCarrusel(eventos, componente, pagina) {
+    let carruselItem = "";
+
     for (let i = 0; i < eventos.length; i++) {
 
         if (i == 0) {
             carruselItem += `<div class="carousel-item active ">
                                 <img src="${eventos[i].image}" class="d-block w-100" alt="Imagen concierto">
                                   <div class="carousel-caption d-none d-md-block">
-                                      <h2>${tiempo}</h2>
+                                      <h2>${pagina}</h2>
                                  </div>
                             </div>`;
         } else {
             carruselItem += `<div class="carousel-item">
                                 <img src="${eventos[i].image}" class="d-block w-100" alt="Imagen concierto">
                                 <div class="carousel-caption d-none d-md-block">
-                                    <h2>${tiempo}</h2>
+                                    <h2>${pagina}</h2>
                                 </div>
                             </div>`;
         }
-
     }
-    return carruselItem;
+    insertarComponente(componente, carruselItem);
 };
 
-/*Funcion que devuelve el HTML para los botones del carrusel*/
-function templateBotoneraCarrusel(eventos) {
+//Funcion: crea el HTML para los botones del carrusel y llama a la funcion para insertarlo
+function templateBotoneraCarrusel(eventos, componente) {
+    let botones = "";
 
     for (let i = 0; i < eventos.length; i++) {
         if (i == 0) {
@@ -66,49 +56,63 @@ function templateBotoneraCarrusel(eventos) {
             aria-label="Slide ${i + 1}"></button>`;
         }
     }
-    return botones;
+    insertarComponente(componente, botones);
+    /* return botones; */
 }
 
-/*Funcion que inserta las tarjetas en el HTML en base a los eventos recibidos*/
-function insertarTarjetas(eventos, contenedorTarjetas) {
-    let tarjetas="";
-    for (let i = 0; i < eventos.length; i++) {
+//Funcion: crea el HTML de las tarjetas y llama a la funcion para insertarlo
+function templateTarjetas(eventos, componente) {
+    let tarjetas = "";
 
-        if (i == 0) {
-            tarjetas += `<div class="card mb-4" style="width: 15rem;">
-                            <img src="${eventos[i].image}" class="card-img-top" alt="Imagen ${eventos[i].name}">
-                            <div class="card-body">
-                                <h5 class="card-title text-center">${eventos[i].name}</h5>
-                                <p class="card-text">${eventos[i].description}</p>
-                                <div class="d-flex justify-content-between">
-                                    <span class="fw-bolder">$ ${eventos[i].price}</span>
-                                    <a href="./details.html" class="btn btn-dark">Details</a>
+    if (eventos.length) {
+        for (let i = 0; i < eventos.length; i++) {
+
+            if (i == 0) {
+                tarjetas += `<div class="card mb-4" style="width: 15rem;">
+                                <img src="${eventos[i].image}" class="card-img-top" alt="Imagen ${eventos[i].name}">
+                                <div class="card-body">
+                                    <h5 class="card-title text-center">${eventos[i].name}</h5>
+                                    <p class="card-text">${eventos[i].description}</p>
+                                    <div class="d-flex justify-content-between">
+                                        <span class="fw-bolder">$ ${eventos[i].price}</span>
+                                        <a href="./details.html?id=${eventos[i]._id}" class="btn btn-dark">Details</a>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>`;
+                            </div>`;
 
-        } else {
+            } else {
 
-            tarjetas += `<div class="card mb-4" style="width: 15rem;">
-                            <img src="${eventos[i].image}" class="card-img-top" alt="Imagen ${eventos[i].name}">
-                            <div class="card-body">
-                                <h5 class="card-title text-center">${eventos[i].name}</h5>
-                                <p class="card-text">${eventos[i].description}</p>
-                                <div class="d-flex justify-content-between">
-                                    <span class="fw-bolder">$ ${eventos[i].price}</span>
-                                    <a href="./details.html" class="btn btn-dark">Details</a>
+                tarjetas += `<div class="card mb-4" style="width: 15rem;">
+                                <img src="${eventos[i].image}" class="card-img-top" alt="Imagen ${eventos[i].name}">
+                                <div class="card-body">
+                                    <h5 class="card-title text-center">${eventos[i].name}</h5>
+                                    <p class="card-text">${eventos[i].description}</p>
+                                    <div class="d-flex justify-content-between">
+                                        <span class="fw-bolder">$ ${eventos[i].price}</span>
+                                        <a href="./details.html?id=${eventos[i]._id}" class="btn btn-dark">Details</a>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>`;
-        }
-    }
+                            </div>`;
+            }
+        };
+    } else {
+        tarjetas += `
+        <div class="alert alert-danger d-flex align-items-center" role="alert">
+            <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+            <div>
+                No se encontraron coincidencias en la búsqueda.
+            </div>
+        </div>`
+    };
+
     //Cargamos las tarjetas.
-    contenedorTarjetas.innerHTML = tarjetas;
+    insertarComponente(componente, tarjetas);
 };
 
-//Funcion q crea el codigo HTML de los checkbox dinamicamente.
-function templateCheckbox(categorias) {
-    console.log(categorias);
+//Funcion: crea el codigo HTML de los checkbox y llama a la funcion para insertarlo.
+function templateCheckbox(componente, categorias) {
+    let checkboxs = "";
+
     for (let i = 0; i < categorias.length; i++) {
         checkboxs += `
         <div class="form-check form-check-inline">
@@ -116,39 +120,94 @@ function templateCheckbox(categorias) {
             <label class="form-check-label" for="${categorias[i]}">${categorias[i]}</label>
         </div>`;
     }
-    return checkboxs;
+    insertarComponente(componente, checkboxs);
 }
 
-/* Ahora creamos el evento para filtrar las tarjetas cuando se selecciona un checkbox.*/
-function filtrarTarjeta(e,eventos,contenedorTarjetas,setchk) {
-    if (e.target.checked) {
-        setchk.add(e.target.value);
-    } else {
-        setchk.delete(e.target.value);
+//Funcion: combina el filtro por checkbox y serchBar y llama funcion insertar las tarjetas
+function filtro(event, eventos, setDeChecks, serchBar, contenedor) {
+
+    if (event.target.type == "checkbox") {
+        setDeChecks = checkBoxSeleccionados(event, setDeChecks)
     };
-    //convierto el Set a Array.
-    let arrayDeChecks = Array.from(setchk);
-    if(arrayDeChecks.length!=0){
-        let tarFilt = eventos.filter((evento) => { return arrayDeChecks.indexOf(evento.category) != -1 });
-        insertarTarjetas(tarFilt,contenedorTarjetas);
-    }else{
-        insertarTarjetas(eventos,contenedorTarjetas);//Si no hay ningun filtro seleccionado muestro todos los checks.
-    };
+    let categoriasChecked = Array.from(setDeChecks);
+    let eventosFiltradoChk = filtrarXcheck(eventos, categoriasChecked);
+    let eventosFiltrado = filtrarXserchBar(serchBar, eventosFiltradoChk);
+    //console.log(arrayFiltradoChk);
+    //console.log(arrayFiltrado);
+    templateTarjetas(eventosFiltrado, contenedor)
+
 };
 
-/*-------------------------------------------------------------------*/
+//FUncion: devuelve un array con los checkbox seleccionados.
+function checkBoxSeleccionados(event, setchk) {
+    if (event.target.checked) {
+        setchk.add(event.target.value);
+    } else {
+        setchk.delete(event.target.value);
+    };
+    return setchk;
+};
 
-/*Colocamos los componentes creados en HTML.*/
-$botoneraCarrusel.innerHTML = templateBotoneraCarrusel(eventos);
-$carruselItem.innerHTML = templateCarrusel(eventos, "HOME");
-$checkbox.innerHTML = templateCheckbox(categoriasFiltrada);
-insertarTarjetas(eventos,$tarjetas);
+// Funcion: Crea un array filtrado de eventos segun los checkbox seleccionados
+function filtrarXcheck(eventos, categoriasChecked) {
+
+    let eventosFiltrados = [];
+
+    if (categoriasChecked.length != 0) {
+        eventosFiltrados = eventos.filter((evento) => { return categoriasChecked.indexOf(evento.category) != -1 });
+    } else {
+        eventosFiltrados = eventos;
+    };
+
+    return eventosFiltrados;
+};
+
+//Funcion: Crea un array filtrado de eventos segun los el texto introduciodo en el serchbar.
+function filtrarXserchBar(componente, eventos) {
+    let arrayFiltrado = [];
+    //console.log([event.target.value]);
+    arrayFiltrado = eventos.filter((evento) => { return evento.name.toLowerCase().startsWith(componente.value.toLowerCase()) });
+    //console.log(arrayFiltrado);
+    return arrayFiltrado;
+};
+
+//Funcion para insertar componentes
+function insertarComponente(component, template) {
+    component.innerHTML = template;
+};
+
+//Funcion para recuperar Array de categorias.
+function obtenerCategorias(eventos) {
+    /* Recuperamos todas las categorias y las guardamos en un array. Para esto usamos la funcion map
+    que me devuelve un array con el return de la funcion flecha */
+    let categorias = eventos.map((evento) => { return evento.category });
+
+    /*Para borrar los duplicados creo un nuevo array con un Set q no permite duplicados pasando 
+    por argunmento el array de categorias. A su ves con el metodo from lo convierto a array nuevamente*/
+    let categoriasFiltrada = Array.from(new Set(categorias));
+
+    return categoriasFiltrada;
+};
+/*----------------------------FIN FUNCIONES-------------------------------*/
 
 
-
-/*------------------------------------------------------------------*/
 /*----------------------------ACTION LISTENERS----------------------------*/
-$checkbox.addEventListener("click", (e)=>{filtrarTarjeta(e,eventos,$tarjetas,setDeChecks)});
+$checkbox.addEventListener("click", (event) => { filtro(event, eventos, setDeChecks, $serchBar, $tarjetas) });
+$serchBar.addEventListener("keyup", (event) => { filtro(event, eventos, setDeChecks, $serchBar, $tarjetas); });
+
+/*----------------------------FIN ACTION LISTENERS------------------------*/
+
+
+/*--------------------LLAMADO FUNCIONES ARRANQUE----------------------------*/
+//Colocamos los componentes creados en HTML
+let categorias = obtenerCategorias(eventos);
+templateCheckbox($checkbox, categorias);
+templateCarrusel(eventos, $carruselItem, "HOME");
+templateBotoneraCarrusel(eventos, $botoneraCarrusel);
+templateTarjetas(eventos, $tarjetas);
+/*--------------------FIN LLAMADO FUNCIONES ARRANQUE-----------------------*/
+
+
 
 
 
